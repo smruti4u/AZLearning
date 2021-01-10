19ddd747f9aa80198b1aa1ca871c1fb3e6daafba
Smruti Ranjan Panda <smrutienator@gmail.com>
2021-01-10 21:35:58 +0530

    Add project files.

---
+++ Azwebapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Azwebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
